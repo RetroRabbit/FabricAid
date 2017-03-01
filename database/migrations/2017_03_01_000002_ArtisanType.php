a8578207392075800be3e491f809966fa05151b3
Tebogo-Seshibe <Tseshibe@retrorabbit.co.za>
2017-03-01 11:23:15 +0200

    Added migrations for tables: issue #1

---
+++ database/migrations/2017_03_01_000002_ArtisanType.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ArtisanType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ArtisanType', function (Blueprint $table) {            
+            $table->increments('Id');
+            $table->string('Name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ArtisanType');
+    }
+}
---
+++ database/migrations/2017_03_01_000005_FocusArea.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FocusArea extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('FocusArea', function (Blueprint $table) {            
+            $table->increments('Id');
+            $table->string('Code');
+            $table->string('Name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('FocusArea');
+    }
+}
