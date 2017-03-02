9a89537bfc4ca09c3c5924a2a1430c4bf3e72403
Tebogo-Seshibe <Tseshibe@retrorabbit.co.za>
2017-03-02 13:36:57 +0200

    Added update functionality: issue #6

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,12 +18,44 @@ class AdminController extends Controller
     // ACTIONS
     public function companies_update(Company $company)
     {
-        return redirect()->route('admin.companies.show');
+        $fields = request()->only('Code', 'Name', 'Description', 'Logo');
+        $validator = validator()->make(request()->except('Submit'),
+        [
+            'Code' => 'required',
+            'Name' => 'required'
+        ]);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator);
+        }
+        else
+        {
+            $company->fill(request()->only('Code', 'Name', 'Description', 'Logo'));
+            $company->save();
+
+            return redirect()->route('admin-companies-show');
+        }
     }
 
     public function companies_delete(Company $company)
     {
-        return redirect()->route('admin.companies.show');
+        $fields = request()->only('Code', 'Name', 'Description', 'Logo');
+        $validator = validator()->make(request()->except('Submit'),
+        [
+            'Code' => 'required|unique:Company',
+            'Name' => 'required'
+        ]);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator);
+        }
+        else
+        {
+            Company::firstOrCreate(request()->only('Code', 'Name', 'Description', 'Logo'));
+            return redirect()->route('admin-companies-show');
+        }
     }
 
     public function companies_create()
--- resources/views/admin/companies/show.blade.php
+++ resources/views/admin/companies/show.blade.php
@@ -5,25 +5,27 @@
 <div class="container" style="margin-top: 100px;">
     <div id="table-header" class="row"> 
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 form-group"> 
-            <p class="col-xs-2"><strong>#</strong></p>
+            <p class="col-xs-1"><strong>#</strong></p>
             <p class="col-xs-2"><strong>Company Code</strong></p>
             <p class="col-xs-2"><strong>Company Name</strong></p>
-            <p class="col-xs-2"><strong>Company Description</strong></p>
+            <p class="col-xs-3"><strong>Company Description</strong></p>
             <p class="col-xs-2"><strong>Company Logo</strong></p>
         </div> 
     </div> 
     
     <div id="table-body" class="row"> 
         @for($i = 0; $i < count($companies); ++$i) 
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 form-group"> 
-            <label class="col-xs-2">{{ $i + 1 }}</label> 
+        <form class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 form-group" method="post" action="{{ route('admin-companies-update', $companies[$i]->Id) }}"> 
+            {{ csrf_field() }} 
+
+            <label class="col-xs-1">{{ $i + 1 }}</label> 
             <input class="col-xs-2" name="Code" type="text" value="{{ $companies[$i]->Code }}" placeholder="JHB-S1"/> 
             <input class="col-xs-2" name="Name" type="text" value="{{ $companies[$i]->Name }}" placeholder="Johanesburg Site 1"/> 
-            <input class="col-xs-2" name="Description" type="text" value="{{ $companies[$i]->Description }}" placeholder="Description..."/> 
+            <input class="col-xs-3" name="Description" type="text" value="{{ $companies[$i]->Description }}" placeholder="Description..."/> 
             <input class="col-xs-2" name="Logo" type="file" value="{{ $companies[$i]->Logo }}" /> 
-            <a class="col-xs-1" href="{{ route('admin-companies-update', $companies[$i]->Id) }}"><i class="fa fa-upload" aria-hidden="true"></i></a> 
+            <button type="submit" name="Submit" class="col-xs-1" style="border: 0; background: none;"><i class="fa fa-upload" aria-hidden="true"></i></button>
             <a class="col-xs-1" href="{{ route('admin-companies-delete', $companies[$i]->Id) }}"><i class="fa fa-ban" aria-hidden="true"></i></a> 
-        </div> 
+        </form> 
         @endfor 
     </div> 
 
@@ -31,10 +33,10 @@
         {{ csrf_field() }} 
 
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 form-group"> 
-            <label class="col-xs-2">New</label> 
+            <label class="col-xs-1">New</label> 
             <input class="col-xs-2" name="Code" type="text" value="{{ old('Code') }}" placeholder="JHB-S1"/> 
             <input class="col-xs-2" name="Name" type="text" value="{{ old('Name') }}" placeholder="Johanesburg Site 1"/> 
-            <input class="col-xs-2" name="Description" type="text" value="{{ old('Description') }}" placeholder="Description..."/> 
+            <input class="col-xs-3" name="Description" type="text" value="{{ old('Description') }}" placeholder="Description..."/> 
             <input class="col-xs-2" name="Logo" type="file" value="{{ old('Logo') }}"> 
             <input class="col-xs-2" type="submit" name="Submit" value="Create" /> 
         <div> 
